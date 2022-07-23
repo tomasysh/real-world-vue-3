340d0d540b13ffecf2aa604300570d9ada0a9420
Tomas <tomasysh@gmail.com>
2022-07-23 15:33:37 +0800

    [Touring Vue Router] 6. Programmatic Navigation

--- src/views/Event/Register.vue
+++ src/views/Event/Register.vue
@@ -1,9 +1,22 @@
 <template>
   <p>Register for the event</p>
+  <button type="button" @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      // Call to API
+      // If registered then redirect to event details
+      this.$router.push({
+        name: 'EventDetails'
+      });
+      // this.$router.replace({
+      //   name: 'EventDetails'
+      // });
+    }
+  }
 }
 </script>
